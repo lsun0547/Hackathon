ad39b219369e224d8e52bb3b8d6bd6fe0d6285b3
lsun0547 <lsun0547@gmail.com>
2025-04-12 13:18:36 -0400

    explanatory comments in app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 from flask import Flask, render_template, request
-import numpy as np
 import joblib
-
 
 app = Flask(__name__)
 
-
-# Load your trained model (Random Forest, for example)
-model = joblib.load('ghost_model.pkl')  # Make sure this path is correct
+model = joblib.load('ghost_model.pkl')  # load model that we have trained using fake data
 
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('index.html') # render html page for our input ui
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST']) # handle form submission from rendered html page
 def predict():
    try:
        # Read input values from form
@@ -31,10 +27,10 @@ def predict():
 
 
        prediction = model.predict([features])[0]
-       result = "Haunted" if prediction == 1 else "Not Haunted"
+       result = "Haunted" if prediction == 1 else "Not Haunted" # based on parameters from user, use ml model to predict if graveyard will be haunted or not
 
 
-       return f"<h2>Prediction: {result}</h2><a href='/'>Back</a>"
+       return f"<h2>Prediction: {result}</h2><a href='/'>Back</a>" # display prediction result
 
 
    except Exception as e:
